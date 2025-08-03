a96ff46c90cb573753f1d99e8010edc89613500a
Othmen <othmhiri@gmail.com>
2025-08-03 20:08:43 +0100

    Fix image URL script paths

--- fix-image-urls.js
+++ fix-image-urls.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({ path: './server/.env' });
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
